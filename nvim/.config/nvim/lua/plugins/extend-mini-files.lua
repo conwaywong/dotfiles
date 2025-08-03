35ae0c3aee08818589be5b0ff51623fec1ccf41b
Conway Wong <8732636+conwaywong@users.noreply.github.com>
2025-08-02 17:26:36 -0700

    Modify config of mini.files.

--- nvim/.config/nvim/lua/plugins/extend-mini-files.lua
+++ nvim/.config/nvim/lua/plugins/extend-mini-files.lua
@@ -25,6 +25,14 @@ return {
     },
   },
   opts = {
+    mappings = {
+      go_in = "<CR>",
+    },
+    windows = {
+      width_nofocus = 20,
+      width_focus = 50,
+      width_preview = 100,
+    },
     options = {
       use_as_default_explorer = true,
     },
