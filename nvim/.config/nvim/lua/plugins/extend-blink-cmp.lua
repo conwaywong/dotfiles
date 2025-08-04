5bd447ed76e9b87a5fd1681a4febadad9a6a5a64
Conway Wong <8732636+conwaywong@users.noreply.github.com>
2025-08-03 18:46:29 -0700

    remap blink keys

---
+++ nvim/.config/nvim/lua/plugins/extend-blink-cmp.lua
@@ -0,0 +1,37 @@
+return {
+  "saghen/blink.cmp",
+  opts = {
+    keymap = {
+      -- your changes here according to blink's docs
+      ["<C-k>"] = { "select_prev", "fallback" },
+      ["<C-j>"] = { "select_next", "fallback" },
+      ["<ESC>"] = { "cancel", "fallback" },
+    },
+    completion = {
+      menu = {
+        draw = {
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                return kind_icon
+              end,
+              -- (optional) use highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                return hl
+              end,
+            },
+            kind = {
+              -- (optional) use highlights from mini.icons
+              highlight = function(ctx)
+                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                return hl
+              end,
+            },
+          },
+        },
+      },
+    },
+  },
+}
