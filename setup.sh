83b730d9f6120ba78bfd6a2dd8d36d71ede779bb
Conway Wong <8732636+conwaywong@users.noreply.github.com>
2025-08-24 09:21:20 -0700

    Update setup.sh
    remove user fonts setup

--- setup.sh
+++ setup.sh
@@ -334,7 +334,7 @@ setup_fonts() {
   log "Setting up fonts..."
 
   # Load user fonts
-  fc-cache -fv
+  # fc-cache -fv
 
   # Install Nerd Font symbols
   sudo wget -P /usr/share/fontconfig/conf.avail/ \
